2944eb83c3fbb6c02b41b0d9160be39e65d1f48e
Anas Bashir <55910194+an4s911@users.noreply.github.com>
2023-03-07 21:25:24 +0300

    Prints a menu, added takeInput function
    - All the functionality in the menu is not ready yet
    - takeInput function does some error handling with the Scanner's input
      function

--- HotelAnas.java
+++ HotelAnas.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class HotelAnas {
     // The FLOORPLAN is a 2D array, in which the rows are floors,
     // and the columns are the rooms.
@@ -9,6 +12,32 @@ public class HotelAnas {
         initRooms(); // Initialize the rooms to have every other room booked by default
 
         System.out.println("---HOTEL MANAGEMENT SYSTEM---");
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean breakTheLoop = false;
+        while (!breakTheLoop) {
+            // Menu
+            System.out.println("\n------------- MENU -------------");
+            System.out.println("1. Print out the floor plan");
+            System.out.println("2. Book a room");
+            System.out.println("3. Free a room");
+            System.out.println("4. Find first free room");
+            System.out.println("5. Find three consecutive rooms");
+            System.out.println("6. Print total number of occupied rooms");
+            System.out.println("0. Exit");
+            System.out.println("--------------------------------\n");
+
+            int choice = takeInput("Enter your choice: ", 6, true, sc);
+
+            if (!breakTheLoop) {
+                System.out.println("Press enter to continue...");
+                sc.nextLine(); // Waits for the user to press enter before showing the menu again
+            }
+
+        }
+
+        sc.close(); // Close the scanner to prevent memory leaks
     }
 
     public static void initRooms() {
@@ -26,5 +55,33 @@ public class HotelAnas {
 
             }
         }
+    }
+
+    public static int takeInput(String prompt, int limit, boolean includeZero, Scanner sc) {
+        // This method is used to take input from the user.
+        // It returns the input as an integer.
+        // It checks if the input is valid by checking if the input is between 0 and the
+        // limit
+
+        System.out.print(prompt);
+        int input;
+        try {
+            input = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Only integers are allowed!");
+            sc.nextLine(); // Clears the input
+            return takeInput(prompt, limit, includeZero, sc);
+        }
+
+        if (input > limit || (includeZero ? input < 0 : input <= 0)) {
+            // Check if the input is greater than the limit
+            // or if the input is less than 0 if inlucdeZero is true (or less than or equal
+            // to 0 if includeZero is false)
+
+            System.out.println("Invalid choice!");
+            return takeInput(prompt, limit, includeZero, sc);
+        }
+
+        return input;
     }
 }
